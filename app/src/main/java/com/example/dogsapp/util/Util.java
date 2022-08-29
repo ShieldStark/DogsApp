669ccaca0d20bf0e692112e1fbb4463a5936b4a2
Piyush <piyushabhiranjan30@gmail.com>
2022-08-29 07:21:15 +0530

    Binding

--- app/src/main/java/com/example/dogsapp/util/Util.java
+++ app/src/main/java/com/example/dogsapp/util/Util.java
@@ -4,6 +4,7 @@ import android.app.DownloadManager;
 import android.content.Context;
 import android.widget.ImageView;
 
+import androidx.databinding.BindingAdapter;
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import com.bumptech.glide.Glide;
@@ -26,5 +27,10 @@ public class Util {
         cpd.setCenterRadius(50f);
         cpd.start();
         return cpd;
+    }
+    @BindingAdapter("android:imageUrl")
+    public static void loadImage(ImageView view ,String url){
+        loadImage(view,url,getProgressDrawable(view.getContext()));
+
     }
 }
--- app/src/main/java/com/example/dogsapp/view/DogsListAdapter.java
+++ app/src/main/java/com/example/dogsapp/view/DogsListAdapter.java
@@ -8,10 +8,12 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dogsapp.R;
+import com.example.dogsapp.databinding.ItemDogBinding;
 import com.example.dogsapp.model.DogBreed;
 import com.example.dogsapp.util.Util;
 
@@ -32,27 +34,32 @@ public class DogsListAdapter extends RecyclerView.Adapter<DogsListAdapter.DogVie
     @NonNull
     @Override
     public DogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dog,parent,false);
+        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+        ItemDogBinding view= DataBindingUtil.inflate(inflater,R.layout.item_dog,parent,false);
+        //View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dog,parent,false);
         return new DogViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DogViewHolder holder, int position) {
-        ImageView image=holder.itemView.findViewById(R.id.imageView);
-        TextView name=holder.itemView.findViewById(R.id.name);
-        TextView lifespan=holder.itemView.findViewById(R.id.lifespan);
-        LinearLayout layout=holder.itemView.findViewById(R.id.dogLayout);
 
-        name.setText(dogsList.get(position).dogBreed);
-        lifespan.setText(dogsList.get(position).lifeSpan);
-        Util.loadImage(image,dogsList.get(position).imageUrl,Util.getProgressDrawable(image.getContext()));
+        holder.itemView.setDog(dogsList.get(position));
 
-        layout.setOnClickListener(view -> {
-            com.example.dogsapp.view.ListFragmentDirections.ActionDetail action=ListFragmentDirections.actionDetail();
-            action.setDogUuid(dogsList.get(position).uuid);
-            Navigation.findNavController(layout).navigate(action);
-        });
+
+        //        ImageView image=holder.itemView.findViewById(R.id.imageView);
+//        TextView name=holder.itemView.findViewById(R.id.name);
+//        TextView lifespan=holder.itemView.findViewById(R.id.lifespan);
+//        LinearLayout layout=holder.itemView.findViewById(R.id.dogLayout);
+//
+//        name.setText(dogsList.get(position).dogBreed);
+//        lifespan.setText(dogsList.get(position).lifeSpan);
+//        Util.loadImage(image,dogsList.get(position).imageUrl,Util.getProgressDrawable(image.getContext()));
+//
+//        layout.setOnClickListener(view -> {
+//            com.example.dogsapp.view.ListFragmentDirections.ActionDetail action=ListFragmentDirections.actionDetail();
+//            action.setDogUuid(dogsList.get(position).uuid);
+//            Navigation.findNavController(layout).navigate(action);
+//        });
 
     }
     @Override
@@ -60,9 +67,9 @@ public class DogsListAdapter extends RecyclerView.Adapter<DogsListAdapter.DogVie
        return dogsList.size();
     }
     class DogViewHolder extends RecyclerView.ViewHolder{
-        public View itemView;
-        public DogViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public ItemDogBinding itemView;
+        public DogViewHolder(@NonNull ItemDogBinding itemView) {
+            super(itemView.getRoot());
             this.itemView=itemView;
         }
     }
